fdbb02090b9553742cc74db0d8218468a9c707fe
Manolo <manololozano93@gmail.com>
2021-02-28 20:34:03 +0100

    refactor(service): move graphql root var to service

--- example-2/backend/src/server.js
+++ example-2/backend/src/server.js
@@ -1,63 +1,8 @@
 const cors = require('cors');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema, GraphQLError } = require('graphql');
-const { Person } = require('./models/person.model');
 const schema = require('./schema/person.schema');
-
-let persons = [];
-
-// Construct a schema, using GraphQL schema language
-
-// The root provides a resolver function for each API endpoint
-const root = {
-  insertPerson({ person }) {
-    persons = [...persons, new Person(person)];
-    return persons;
-  },
-  findAll() {
-    return persons;
-  },
-  findById({ id }) {
-    const person = persons.filter(_person => +_person.id === +id);
-    if (!person) {
-      throw new GraphQLError('ERROR: No existe ninguna persona con este ID: ' + id);
-    }
-    return person;
-  },
-  findByName({ name }) {
-    const person = persons.filter(_person => _person.name.includes(name));
-    if (!person) {
-      throw new GraphQLError('ERROR: No existe ninguna persona con este Nombre: ' + name);
-    }
-    return person;
-  },
-  findByAge({ age }) {
-    const person = persons.filter(_person => +_person.age === +age);
-    if (!person) {
-      throw new GraphQLError('ERROR: No existe ninguna persona con esta edad: ' + age);
-    }
-    return person;
-  },
-  updatePerson({ id, person }) {
-    const personToUpdate = persons.find(_person => +_person.id === +id);
-    const found = {
-      true: () => {
-        Object.assign(personToUpdate, person);
-      },
-      false: () => {
-        throw new GraphQLError('ERROR: Persona no encontrada');
-      },
-    };
-    found[new Boolean(personToUpdate)]();
-    return persons;
-  },
-  deletePerson({ id }) {
-    const personsFiltered = persons.filter(person => person.id != id);
-    persons = personsFiltered;
-    return persons;
-  },
-};
+const root = require("./services/crud");
 
 const app = express();
 app.use(cors());
